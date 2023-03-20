c3077591189e5f01e45ebc42e72e4c1e81e81017
Nathan Cassee <n.w.cassee@tue.nl>
2023-03-20 13:53:33 +0100

    Fix saving of model

--- app.py
+++ app.py
@@ -277,7 +277,7 @@ def train(file_name, text_column = "text", label_column = "polarity", stratified
     
     print(classification_report(output_df['GroundTruth'], output_df['Prediction']))
 
-    model.save_model(f'models/bert_{model_name}.model')
+    model.save_pretrained(f'models/bert_{model_name}/')
 
     np.save(f'models/encoder_{model_name}.npy', encoder.classes_)
 
